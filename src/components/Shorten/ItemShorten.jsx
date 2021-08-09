09fd5c42eb122a1500a1036065808c8a7390331e
leonardo sifuentes <lsifuentes@avatar-global.com>
2021-08-09 18:16:46 -0500

    fecth

--- src/components/Shorten/ItemShorten.jsx
+++ src/components/Shorten/ItemShorten.jsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
+
 export const ItemShorten = ({ value }) => {
   // code: rest.code,
   // original_link: rest.original_link,
   // short_link: rest.full_short_link2,
-  const { code, original_link, short_link } = value;
+  const { original_link, short_link } = value;
+  const shortUrl = useRef();
+  const handleClick = async () => {
+    try {
+      console.log(shortUrl.current.innerHTML);
+
+      // document.execCommand("copy");
+      await navigator.clipboard.writeText(shortUrl.current.innerHTML);
+      alert("copied!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="link-item">
       <span className="url-full">{original_link}</span>
-      <span className="url-short">{short_link}</span>
-      <button>Copied!</button>
+      <span className="url-short" ref={shortUrl}>
+        {short_link}
+      </span>
+      <button onClick={handleClick}>Copied!</button>
     </div>
   );
 };
